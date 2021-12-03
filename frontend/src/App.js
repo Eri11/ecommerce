633c02a56f63d7c1418fb2d9bf1f37e3b31e3cf4
Eri11 <erikaeli11@gmail.com>
2021-12-03 04:54:54 -0700

    products shown from JSON file successfully

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,9 @@
 import { Container } from 'react-bootstrap'
+
 import Header from './components/Header'
 import Footer from './components/Footer'
+
+import HomeScreen from './screens/HomeScreen'
 
 
 function App() {
@@ -9,9 +12,8 @@ function App() {
       <Header />
       <main className="py-3">
         <Container>
-          <h1>
-            Welcome
-          </h1> </Container>
+          <HomeScreen />
+        </Container>
       </main>
       <Footer />
     </div>
