222a1acf2236e05ba9591c4ec71f2e671a52e339
Eri11 <erikaeli11@gmail.com>
2021-12-08 22:20:49 -0700

    shipping shows on redux

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -14,7 +14,7 @@ const reducer = combineReducers({
     userLogin: userLoginReducer,
     userRegister: userRegisterReducer,
     userDetails: userDetailsReducer,
-    userUpdateProfile: userUpdateProfileReducer,
+
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems') ?
@@ -23,8 +23,15 @@ const cartItemsFromStorage = localStorage.getItem('cartItems') ?
 const userInfoFromStorage = localStorage.getItem('userInfo') ?
     JSON.parse(localStorage.getItem('userInfo')) : null
 
+const shippingAddressFromStorage = localStorage.getItem('shippingAddress') ?
+    JSON.parse(localStorage.getItem('shippingAddress')) : {}
+
+
 const inistialState = {
-    cart: { cartItems: cartItemsFromStorage },
+    cart: {
+        cartItems: cartItemsFromStorage,
+        shippingAddress: shippingAddressFromStorage
+    },
     userLogin: { userInfo: userInfoFromStorage },
 
 }
